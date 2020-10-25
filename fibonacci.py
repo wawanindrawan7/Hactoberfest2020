0ee9671fc1e8378ae886113b3c00374baa05d835
rahmah18 <56541229+rahmah18@users.noreply.github.com>
2020-10-25 10:18:08 +0100

    Update fibonacci.py

--- fibonacci.py
+++ fibonacci.py
@@ -7,3 +7,14 @@ while(i<=n):
     x = l[i-1] + l[i-2]
     l.append(x)
 print(l[l.len-1])
+
+def fib():
+     n = int(input())
+     a = 0
+     b = 1
+     p = []
+     for i in range(n):
+         p.append(a)
+         a,b = b,a+b
+     print(p)
+fib()
